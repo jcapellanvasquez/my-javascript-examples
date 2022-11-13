9db3b1b34301633bcef21db0f7294033c4d3a49e
jcapellan <jv.capellan06@gmail.com>
2022-11-13 14:14:26 -0400

    async example - vercel config needed it

--- async/index.js
+++ async/index.js
@@ -5,3 +5,5 @@ const port = 8100;
 app.get("/", (req, res) => res.send("Running on express"))
 
 app.listen(port,console.log(`Application running on port: ${port}: go to http://localhost:${port}`));
+
+module.exports = app;
