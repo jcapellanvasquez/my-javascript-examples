222be99d0a1ea641cc64a376d3ba0a83b18d4089
jcapellan <jv.capellan06@gmail.com>
2022-11-14 17:26:14 -0400

    change port

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8100;
+const port = 80;
 
 app.use("/assets", express.static(path.resolve(__dirname, "assets")));
 
 app.get("/*", (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')))
 
 app.listen(port, console.log(`Application is running on port: ${port}: go to http://localhost:${port}`));
-
-module.exports = app;
