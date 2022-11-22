5d6c032370ea022429d9c9402563eb1bf4b43c4e
jcapellanvasquez <jv.capellan06@gmail.com>
2022-11-21 18:24:39 -0800

    update dom import

--- assets/js/config/routes.js
+++ assets/js/config/routes.js
@@ -4,14 +4,14 @@ import Async from "../views/async/Async.js";
 import AsyncAwait from "../views/async/AsyncAwait.js";
 import ES6Modules from "../views/ES6Modules.js";
 import Fetch from "../views/async/Fetch.js";
-import dom from "../views/dom.js";
+import Dom from "../views/Dom.js";
 
 export const routes = [
-  { path: "/", template: Welcome },
-  { path: "/modules", template: ES6Modules },
-  { path: "/async", template: Async },
-  { path: "/promise", template: Promises },
-  { path: "/async-await", template: AsyncAwait },
-  { path: "/fetch", template: Fetch },
-  { path: '/dom', template: dom}
+    { path: "/", template: Welcome },
+    { path: "/modules", template: ES6Modules },
+    { path: "/async", template: Async },
+    { path: "/promise", template: Promises },
+    { path: "/async-await", template: AsyncAwait },
+    { path: "/fetch", template: Fetch },
+    { path: "/dom", template: Dom },
 ];
